d66bf0784ef6b74ae2cc167b13e59f565e3c170d
João Alencar <ra197286@ucdb.br>
2025-03-12 20:11:38 -0300

    teste
    despresar commit

---
+++ Q5.cpp
@@ -0,0 +1,8 @@
+/*5 - Escreva um programa com um procedimento que receba três notas e uma letra como parâmetros. Se a letra for “A”, calcule e retorne a média aritmética. Se for “P”, retorne a média ponderada considerando os pesos 2, 3 e 4 para as respectivas notas.*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello Word";
+}
